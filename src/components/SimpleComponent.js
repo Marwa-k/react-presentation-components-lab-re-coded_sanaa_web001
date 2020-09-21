90996bcbb712a12ab21d2b3b96d445a0a5fb9909
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-21 11:38:46 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -9,12 +9,12 @@ class SimpleComponent extends React.Component {
     }
 
     handleClick = () => {
-       if (this.state.mood === 'happy') {        
+       if (this.state.mood === 'happy') {
            this.setState({ mood: 'sad' })
        } else {
            this.setState({ mood: 'happy '})
        }
-    } 
+    }
 
     render() {
         return <div onClick={this.handleClick}>{this.state.mood}</div>;
